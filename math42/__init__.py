999199ef0dec3e52538a92a97fdeee4da8d2d262
CarlosDCpycpp <carlosdanielgcunha@gmail.com>
2026-06-26 23:15:10 +0100

    Reorganization of __init__.py Internal Imports

--- math42/__init__.py
+++ math42/__init__.py
@@ -8,8 +8,9 @@ from .constants import *
 from .convert import *
 from .convertion_to_spec_base import *
 from .fibonacci import *
-from .funcs import *
+from .misc_funcs import *
 from .generator import *
 from .infinity import *
 from .math_function import *
 from .number_range import *
+from .percentages import *
--- test0.py
+++
@@ -1,5 +0,0 @@
-import time
-start = time.perf_counter()
-from math42 import *  # NoQA
-end = time.perf_counter()
-print("import time:", end - start)
